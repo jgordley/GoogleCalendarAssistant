8784d7f0dc6d4ec764de4caab8534960518f926a
jack <jack@telnyx.com>
2023-10-18 01:20:57 -0500

    Frontend chat window improvements

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -7,7 +7,7 @@ import { fetchPrimaryCalendarEvents, fetchCalendarList, fetchPrimaryChat } from 
 
 
 export default function Chat() {
-    const { status, data:session } = useSession();
+    const { status, data: session } = useSession();
 
     const [messages, setMessages] = useState([
         { user: "User2", text: "Hi! My name is Callie, your Google Calendar Assistant. What can I help you with?", color: "green" },
@@ -16,8 +16,16 @@ export default function Chat() {
     const [currentMessage, setCurrentMessage] = useState('');
     const messagesRef = useRef(null);
 
+    const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+        }
+    }, [messages]);  // assuming `messages` is the array of messages
+
     function renderWithLineBreaks(text: string) {
-        const newText = text.split('\n').map((str, index, array) => 
+        const newText = text.split('\n').map((str, index, array) =>
             index === array.length - 1 ? str : <>
                 {str}
                 <br />
@@ -25,7 +33,12 @@ export default function Chat() {
         );
         return newText;
     }
-    
+
+    // TODO: Make this into a function instead of setting new message state directly in each handler
+    // const updateMessages = async (newMessage: any) => {
+
+    // }
+
 
     const handleSendMessage = async (e: any) => {
 
@@ -34,46 +47,55 @@ export default function Chat() {
 
         if (currentMessage.trim() !== '') {
             setMessages(prevMessages => [
-                { user: "User1", text: currentMessage, color: "blue" }, 
-                ...prevMessages
+                ...prevMessages,
+                { user: "User1", text: currentMessage, color: "blue" }
+
             ]);
             setCurrentMessage('');
         }
 
         // Send a request to the backend
-        
+
         if (currentMessage.includes('calendars')) {
             let email: string = session?.user?.email || '';
             let calendars = await fetchCalendarList(email);
             console.log(calendars);
-            
+
             const calendarNames = calendars.calendar_names.map((calendar: any) => Object.values(calendar)[0]);
             const resultString = `Calendar Names: ${calendarNames.join(', ')}`;
-        
-            setMessages(prevMessages => [
-                { user: "User2", text: resultString, color: "green" },
-                ...prevMessages
-            ]);
-        }
 
-        if (currentMessage.includes('events')) {
+            setMessages(prevMessages => [
+                ...prevMessages,
+                { user: "User2", text: resultString, color: "green" }
+            ]);
+        } else if (currentMessage.includes('events')) {
             let email: string = session?.user?.email || '';
             let events = await fetchPrimaryCalendarEvents(email);
             console.log(events);
-            
+
             const eventNames = events.events;
             const resultString = `Upcoming Events:\n ${eventNames.join('\n ')}`;
-        
+
             setMessages(prevMessages => [
-                { user: "User2", text: resultString, color: "green" },
-                ...prevMessages
+                ...prevMessages,
+                { user: "User2", text: resultString, color: "green" }
+            ]);
+        } else {
+            // Append a random string message from user2 without fetching
+            const randomString = Math.random().toString(36).substring(7);
+            setMessages(prevMessages => [
+                ...prevMessages,
+                { user: "User2", text: randomString, color: "green" }
             ]);
         }
+
+
     }
 
     return (
         <div className="flex flex-col h-screen">
-            <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
+            {/* <div className="flex flex-col h-screen"> */}
+            <header className="flex justify-between items-center p-4 bg-gray-800 text-white flex-shrink-0">
                 <div className="flex items-center">
                     <CalendarIcon className="h-8 w-8 text-white" />
                     <h1 className="ml-2 text-2xl">Google Calendar Assistant</h1>
@@ -81,36 +103,63 @@ export default function Chat() {
                 <SignInButton />
             </header>
 
-            <main className="flex-1 flex flex-col">
-
-                <div className="flex-grow flex flex-col-reverse bg-white p-4 overflow-y-auto" ref={messagesRef}>
-                    {messages.map((msg, idx) => (
-                        <div key={idx} className="mb-4">
-                            <div className={`font-semibold text-${msg.color}-500`}>{msg.user}:</div>
-                            <div className={`bg-${msg.color}-100 p-2 rounded mt-1`}>
-                                {renderWithLineBreaks(msg.text)}
+            <main className="flex-1 flex flex-col overflow-hidden">
+                <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-80 bg-white">
+                    <div id="messages" ref={messagesEndRef} className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
+                        {messages.map((msg, idx) => (
+                            msg.user === "User1" ? (
+                                <div>
+                                    <div className="flex items-end justify-end">
+                                        <div className="flex flex-col space-y-2 text-md leading-tight max-w-lg mx-2 order-1 items-end">
+                                            <div>
+                                                <span className="px-4 py-3 rounded-xl inline-block rounded-br-none bg-blue-500 text-white">{msg.text}</span>
+                                            </div>
+                                        </div>
+                                        <img src="https://i.pravatar.cc/100?img=7" alt="" className="w-6 h-6 rounded-full order-2"></img>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div>
+                                    <div className="flex items-end">
+                                        <div className="flex flex-col space-y-2 text-md leading-tight max-w-lg mx-2 order-2 items-start">
+                                            <div>
+                                                <span className="px-4 py-3 rounded-xl inline-block rounded-bl-none bg-gray-100 text-gray-600">{msg.text}</span>
+                                            </div>
+                                        </div>
+                                        <img src="https://cdn.icon-icons.com/icons2/1371/PNG/512/robot02_90810.png" alt="" className="w-6 h-6 rounded-full order-1"></img>
+                                    </div>
+                                </div>
+                            )
+                        )
+                        )}
+                        <div x-show="botTyping" style={{ display: "none" }}>
+                            <div className="flex items-end">
+                                <div className="flex flex-col space-y-2 text-md leading-tight mx-2 order-2 items-start">
+                                    <div><img src="https://support.signal.org/hc/article_attachments/360016877511/typing-animation-3x.gif" alt="..." className="w-16 ml-6"></img></div>
+                                </div>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                    <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+                        <form onSubmit={handleSendMessage}>
+                            <div className="relative flex">
+                                <input type="text" value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} placeholder="Say something..." autoFocus={true} autoCorrect="false" className="text-md w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-5 pr-16 bg-gray-100 border-2 border-gray-200 focus:border-blue-500 rounded-full py-2" x-ref="input" />
+                                <div className="absolute right-2 items-center inset-y-0 hidden sm:flex">
+                                    <button type="submit" className="inline-flex items-center rounded-full justify-center transition duration-200 ease-in-out text-white bg-blue-500 hover:bg-blue-600 focus:outline-none">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
 
-                <div className="p-6">
-                    <form onSubmit={handleSendMessage}>
-                        <input
-                            value={currentMessage}
-                            onChange={(e) => setCurrentMessage(e.target.value)}
-                            className="w-full p-2 border rounded"
-                            placeholder="Type a message..."
-                        />
-                        <button
-                            type="submit"  // This is important for form behavior
-                            className="mt-2 w-full bg-blue-500 text-white p-2 rounded">
-                            Send
-                        </button>
-                    </form>
+
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
 
             </main>
+            {/* </div> */}
         </div>
     );
 }
